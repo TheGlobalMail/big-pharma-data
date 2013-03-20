610fca1b47cd581999ff1f0ed900678b0093e080
B.J. Rossiter <b.j.rossiter@gmail.com>
2013-03-21 10:49:03 +1100

    Added loading overlay

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,13 @@
-(function() {
+(function($, loadingOverlay, stats) {
   'use strict';
 
+  // Load the stats when the page is ready
+  $(populateStats);
 
-}());
+  // Render the stats into the html and dismiss loading overlay when done
+  function populateStats(){
+    loadingOverlay.dismiss();
+  }
+
+
+}($, window.loadingOverlay, window.stats));
